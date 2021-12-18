9ace3c5c27906bfd345887f0b3a96d03007776b1
Mihail Bogdanchikov <mihailBogdanchikovWork@gmail.com>
2021-12-18 18:18:00 +0300

    validations tests added

--- src/test/java/com/jinquiet/validator/impl/AgeValidatorTest.java
+++ src/test/java/com/jinquiet/validator/impl/AgeValidatorTest.java
@@ -1,4 +1,39 @@
+package com.jinquiet.validator.impl;
+
+import com.jinquiet.validator.ValidationResult;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class AgeValidatorTest {
-  
+
+    @Test
+    public void validateGetValidResult() {
+        boolean expected = ValidationResult.valid().isValid();
+        boolean actual = new AgeValidator().validate("123").isValid();
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void validateGetInvalidResult() {
+        boolean expected = ValidationResult.invalid("Illegal string").isValid();
+        boolean actual = new AgeValidator().validate("").isValid();
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void validateGetInvalidResultLongString() {
+        boolean expected = ValidationResult.invalid("Illegal string").isValid();
+        boolean actual = new AgeValidator().validate("1111111").isValid();
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void validateGetInvalidResultStringWithoutChars() {
+        boolean expected = ValidationResult.invalid("Illegal string").isValid();
+        boolean actual = new AgeValidator().validate("      ").isValid();
+        assertEquals(expected,actual);
+    }
 }
--- src/test/java/com/jinquiet/validator/impl/IntegerValidatorTest.java
+++ src/test/java/com/jinquiet/validator/impl/IntegerValidatorTest.java
@@ -1,4 +1,30 @@
+package com.jinquiet.validator.impl;
+
+import com.jinquiet.validator.ValidationResult;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class IntegerValidatorTest {
-  
+
+    @Test
+    public void validateGetValidResult() {
+        boolean expected = ValidationResult.valid().isValid();
+        boolean actual = new IntegerValidator().validate("1").isValid();
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void validateGetInvalidResult() {
+        boolean expected = ValidationResult.invalid("Not integer").isValid();
+        boolean actual = new IntegerValidator().validate("sss").isValid();
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void validateEmptyArgument() {
+        boolean expected = ValidationResult.invalid("Not integer").isValid();
+        boolean actual = new IntegerValidator().validate("").isValid();
+        assertEquals(expected,actual);
+    }
 }
